b3dd3103addfc7093a5bb1c6a98fcf0b4fa7f404
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-07-07 18:39:26 +0300

    setup project

---
+++ webpack.base.conf.js
@@ -0,0 +1,122 @@
+const path = require("path");
+const fs = require("fs");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+  src: path.resolve(__dirname, "src"),
+  dist: path.resolve(__dirname, "dist"),
+  assets: "assets/",
+};
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith(".pug"));
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    main: PATHS.src,
+    slider: PATHS.src + "/js/slider.js",
+  }, 
+  output: {
+    filename: "[name].js",
+    path: PATHS.dist,
+    clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/i,
+        loader: "pug3-loader",
+      },
+
+      {
+        test: /\.js$/i,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[hash].[ext]",
+        },
+      },
+
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    alias: {
+      "~": PATHS.src,
+    },
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `[name].css`,
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+        { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts`, },
+        { from: `${PATHS.src}/static`, to: "static" },
+      ],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/index.pug`,
+      filename: "./index.html",
+      chunks: ['main'],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/feedback.pug`,
+      filename: "./feedback.html",
+      chunks: ['main'],
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/record.pug`,
+      filename: "./record.html",
+      chunks: ['main'],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/records.pug`,
+      filename: "./records.html",
+      chunks: ['main', 'slider'],
+    }),    
+  ],
+};
